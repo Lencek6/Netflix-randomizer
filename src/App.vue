f50a131cf7909bc3df2b8ea6145f95b5c78fd5c0
Lencek6 <lencek113@gmail.com>
2019-10-26 15:56:10 +0200

    navi fix

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,39 @@
 <template>
-  <div>
-  <div class="ml-5 mt-1">
-  <Navigation></Navigation>
-  </div>
-  <div>
-    <router-view></router-view>
-  </div>
-  </div>
+    <div>
+        <Navigation></Navigation>
+        <div>
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 
 <script>
-import Navigation from "@/components/Navigation";
-export default {
-  name: 'app',
-  components: {
-    Navigation
-  }
-}
+    import Navigation from "@/components/Navigation";
+
+    export default {
+        name: 'app',
+        components: {
+            Navigation
+        }
+    }
 </script>
 
 <style>
 
     body {
-        background: url('../public/netflixlogo.jpg')no-repeat center center fixed;
+        background: url('../public/netflixlogo.jpg') no-repeat center center fixed;
         -webkit-background-size: cover;
         -moz-background-size: cover;
         -o-background-size: cover;
         background-size: cover;
     }
-    .v-align {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 90vh;
-    }
 
+    .v-align {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 90vh;
+    }
 
 
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,17 +1,24 @@
 <template>
-    <div>
-        <!-- Single button triggers two "<b-collapse>" components -->
-        <b-button class="btn-navi" variant="danger" v-b-toggle.collapse-a.collapse-b><unicon name="sort-amount-down" fill="black"></unicon></b-button>
-        <div id="inline">
-        <!-- Elements to collapse -->
-        <b-collapse id="collapse-a" class="ml-5">
-            <router-link to="/randomizer" style="color: white">Randomizer</router-link>
-        </b-collapse>
-        <b-collapse id="collapse-b" class="ml-5">
-            <router-link to="/history" style="color: white">History</router-link>
-        </b-collapse>
-        </div>
-    </div>
+    <b-container class="mt-2">
+        <b-row align-v="center">
+            <b-col class="col-2 col-md-2 col-lg-2 col-xl-2 mr-4">
+                <b-button class="btn-navi" variant="danger" v-b-toggle.collapse-navi>
+                    <unicon name="sort-amount-down" fill="black"></unicon>
+                </b-button>
+            </b-col>
+            <b-col class="col-4 col-md-4 col-lg-4 col-xl-4">
+                <b-collapse id="collapse-navi">
+                    <router-link to="/randomizer" style="color: white">Randomizer</router-link>
+                </b-collapse>
+            </b-col>
+            <b-col class="col-4 col-md-4 col-lg-4 col-xl-4">
+                <b-collapse id="collapse-navi">
+                    <router-link to="/history" style="color: white">History</router-link>
+                </b-collapse>
+            </b-col>
+
+        </b-row>
+    </b-container>
 </template>
 
 <script>
@@ -21,18 +28,20 @@
 </script>
 
 <style scoped>
-  #inline{
-    display: inline-flex;
-  }
-  .btn-navi{
-      border: none !important;
-      outline: 0;
-      background: rgba(255, 0, 3, 0.83);
-  }
-  .btn-navi:focus,.btn-navi:active{
-      outline: none !important;
-      box-shadow: none !important;
-      border: none !important;
-      background: rgba(255, 0, 3, 0.83);
-  }
+    #inline {
+        display: inline-flex;
+    }
+
+    .btn-navi {
+        border: none !important;
+        outline: 0;
+        background: rgba(255, 0, 3, 0.83);
+    }
+
+    .btn-navi:focus, .btn-navi:active {
+        outline: none !important;
+        box-shadow: none !important;
+        border: none !important;
+        background: rgba(255, 0, 3, 0.83);
+    }
 </style>
